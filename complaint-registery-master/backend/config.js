27f429eb4ebc5837e5020f84bd956124f0d9453f
Nivedithanive <146552485+Nivedithanive@users.noreply.github.com>
2024-11-19 12:06:57 +0530

    Delete complaint-registery-master directory

--- complaint-registery-master/backend/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://nivedd2003:Nividitha007@complaintregistery.fzolt.mongodb.net/?retryWrites=true&w=majority&appName=ComplaintRegistery")
-.then(()=>{
-   console.log("connected to mongodb")
-})
